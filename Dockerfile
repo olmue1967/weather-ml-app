a9d3d091316c81b51ece6d4e991764772ca0ad28
HKHANB301 <HKHANB301@CALEDONIAN.AC.UK>
2025-11-20 15:15:36 +0000

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,11 @@
-# Use an official Python runtime as a parent image
-FROM python:3.10-slim
+FROM python:3.9-slim
 
 # Set the working directory in the container
-WORKDIR /usr/src/app
 
-# Copy the requirements file and install dependencies
-COPY requirements.txt ./
-RUN pip install --no-cache-dir -r requirements.txt
 
-# Copy the application code
-COPY app.py .
+# Install dependencies
 
-# Expose the port the app runs on
-EXPOSE 5000
+# Copy the rest of the application code
 
-# Run the application using gunicorn for production
-CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
+# Run the app
+
--- app.py
+++ app.py
@@ -1,20 +1,58 @@
-from flask import Flask, jsonify
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    # Placeholder response
-    return "<h1>Weather ML App is Running!</h1><p>Placeholder page for Task 1 completion.</p>"
+weather_classes = ['clear', 'cloudy', 'drizzly', 'foggy', 'hazey', 'misty', 'rainy', 'smokey', 'thunderstorm']
 
-@app.route('/predict')
-def predict():
-    # Placeholder prediction endpoint
-    data = {
-        "status": "success",
-        "model_result": "Simulated sunny day",
-    }
-    return jsonify(data)
+def load_model(model_path = 'model/model.pkl'):
+	return pickle.load(open(model_path, 'rb'))
+
+def classify_weather(features):
+	model = load_model()
+	start = time.time()
+	prediction_index = model.predict(features)[0]
+	latency = round((time.time() - start) * 1000, 2) #we are here
+	prediction = weather_classes[1]
+	
+	return prediction, latency
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		try:
+			# Extract floats from form data
+			temperature = request.form['temperature']
+			pressure = request.form['pressure']
+			humidity = request.form['humidity']
+			wind_speed = request.form['wind_speed']
+			wind_deg = request.form['wind_deg']
+			rain_1h = float(request.form.get('rain_1h', 0) or 0)
+			rain_3h = float(request.form.get('rain_3h', 0) or 0)
+			snow = float(request.form.get('snow', 0) or 0)
+			clouds = float(request.form.get('clouds', 0) or 0)
+
+			features = np.array([
+				temperature, pressure, humidity,
+				wind_speed, wind_deg, rain_1h,
+				rain_3h, snow, clouds
+			]).reshape(1, -1)
+
+			
+			prediction, latency = classify_weather(features)
+
+
+			return render_template('result.html', prediction=prediction, latency=latency)
+
+		except Exception as e:
+			error_msg = f"Error processing input: {e}"
+			return render_template('form.html', error=error_msg)
+	# GET method: show the input form
+	return render_template('form.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+	app.run(host="0.0.0.0", port=5000)
